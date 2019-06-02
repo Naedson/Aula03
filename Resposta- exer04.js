5f207975b112a27c91c871d20e277aeb8c4abf5a
Naedson <naedson77@gmail.com>
2019-06-02 17:56:30 -0300

    Conclusão do Exercício 4

---
+++ Resposta- exer04.js
@@ -0,0 +1,11 @@
+var idade = 39
+
+if (idade >= 0 && idade < 12) {
+        console.log("Criança")
+} else if (idade >= 12 && idade < 18) {
+        console.log("Adolescente")
+} else if (idade >= 18 && idade < 40) {
+        console.log("Adulto")
+} else if (idade > 40) {
+        console.log("Velho")
+}
